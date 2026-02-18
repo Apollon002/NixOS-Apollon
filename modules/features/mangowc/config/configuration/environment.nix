71c22d22fbe473a7e28385d9d7444fd8ec6ea82a
Apollon002 <accounts@jannikzenker.de>
2026-02-18 14:59:50 +0100

    Fixed explicit sync bug for mangowc
    - Fix: Fixed a bug that caused kitty to crash mangowc while explicit
    sync is enabled.

---
+++ modules/features/mangowc/config/configuration/environment.nix
@@ -0,0 +1,16 @@
+{ ... }:
+{
+  flake.modules = {
+    nixos.mango =
+      { config, lib, ... }:
+      {
+        environment.sessionVariables = lib.mkIf config.hardware.nvidia.modesetting.enable {
+          WLR_DRM_NO_ATOMIC = 1;
+        };
+      };
+
+    homeManager.mango = {
+      wayland.windowManager.mango.settings = "";
+    };
+  };
+}
