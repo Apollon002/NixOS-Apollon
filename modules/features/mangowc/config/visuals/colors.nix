639b56a50a332ffd41ffb0b3cdca6dc04a1f5df8
Apollon002 <accounts@jannikzenker.de>
2026-02-19 16:02:59 +0100

    Added automatic theming for mangowc by dms
    - Feat: Dank Material Shell is now able to automatically theme mangowc
    windows

--- modules/features/mangowc/config/visuals/colors.nix
+++ modules/features/mangowc/config/visuals/colors.nix
@@ -1,11 +1,16 @@
 {
-  flake.modules.homeManager.mango = {
-    wayland.windowManager.mango.settings = ''
-      focuscolor=0xd6afffff
-      maximizescreencolor=0x5e5ad8ff
-      scratchpadcolor=0x5e5ad8ff
-      bordercolor=0x505050ff
-      urgentcolor=0x9b0000ff
-    '';
-  };
+  flake.modules.homeManager.mango =
+    { config, lib, ... }:
+    {
+      wayland.windowManager.mango.settings = ''
+        focuscolor=0xd6afffff
+        maximizescreencolor=0x5e5ad8ff
+        scratchpadcolor=0x5e5ad8ff
+        bordercolor=0x505050ff
+        urgentcolor=0x9b0000ff
+      ''
+      + lib.optionalString config.programs.dank-material-shell.enable ''
+        source=./dms/colors.conf
+      '';
+    };
 }
