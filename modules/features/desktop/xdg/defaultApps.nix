e23f6c2c6d097894b108065323f0de75cfb53cf8
Apollon002 <accounts@jannikzenker.de>
2026-02-19 22:10:38 +0100

    Added xdg user dir creation
    - Feat: XDG now creates user dirs

--- modules/features/desktop/xdg/defaultApps.nix
+++ modules/features/desktop/xdg/defaultApps.nix
@@ -14,86 +14,89 @@
       office = "${config.userSettings.defaultOffice}.desktop";
     in
     {
-      xdg.mimeApps = {
-        enable = true;
-        defaultApplications = {
-          "x-scheme-handler/http" = [ browser ];
-          "x-scheme-handler/https" = [ browser ];
-          "x-scheme-handler/about" = [ browser ];
-          "x-scheme-handler/unknown" = [ browser ];
-          "text/html" = [ browser ];
-          "application/xhtml+xml" = [ browser ];
+      xdg = {
+        mime.enable = true;
+        mimeApps = {
+          enable = true;
+          defaultApplications = {
+            "x-scheme-handler/http" = [ browser ];
+            "x-scheme-handler/https" = [ browser ];
+            "x-scheme-handler/about" = [ browser ];
+            "x-scheme-handler/unknown" = [ browser ];
+            "text/html" = [ browser ];
+            "application/xhtml+xml" = [ browser ];
 
-          "x-scheme-handler/mailto" = [ mail ];
-          "message/rfc822" = [ mail ];
+            "x-scheme-handler/mailto" = [ mail ];
+            "message/rfc822" = [ mail ];
 
-          "inode/directory" = [ fm ];
+            "inode/directory" = [ fm ];
 
-          "text/plain" = [ editor ];
-          "text/markdown" = [ editor ];
-          "application/json" = [ editor ];
-          "application/xml" = [ editor ];
-          "text/x-shellscript" = [ editor ];
-          "text/x-python" = [ editor ];
-          "text/x-c" = [ editor ];
-          "text/x-c++" = [ editor ];
-          "text/x-rust" = [ editor ];
-          "text/x-nix" = [ editor ];
-          "application/x-yaml" = [ editor ];
-          "text/yaml" = [ editor ];
-          "text/x-toml" = [ editor ];
+            "text/plain" = [ editor ];
+            "text/markdown" = [ editor ];
+            "application/json" = [ editor ];
+            "application/xml" = [ editor ];
+            "text/x-shellscript" = [ editor ];
+            "text/x-python" = [ editor ];
+            "text/x-c" = [ editor ];
+            "text/x-c++" = [ editor ];
+            "text/x-rust" = [ editor ];
+            "text/x-nix" = [ editor ];
+            "application/x-yaml" = [ editor ];
+            "text/yaml" = [ editor ];
+            "text/x-toml" = [ editor ];
 
-          "application/pdf" = [ pdf ];
-          "application/x-pdf" = [ pdf ];
+            "application/pdf" = [ pdf ];
+            "application/x-pdf" = [ pdf ];
 
-          "image/jpeg" = [ image ];
-          "image/jpg" = [ image ];
-          "image/png" = [ image ];
-          "image/webp" = [ image ];
-          "image/gif" = [ image ];
-          "image/svg+xml" = [ image ];
-          "image/tiff" = [ image ];
-          "image/bmp" = [ image ];
-          "image/x-xcf" = [ image ];
+            "image/jpeg" = [ image ];
+            "image/jpg" = [ image ];
+            "image/png" = [ image ];
+            "image/webp" = [ image ];
+            "image/gif" = [ image ];
+            "image/svg+xml" = [ image ];
+            "image/tiff" = [ image ];
+            "image/bmp" = [ image ];
+            "image/x-xcf" = [ image ];
 
-          "video/mp4" = [ video ];
-          "video/x-matroska" = [ video ]; # mkv
-          "video/webm" = [ video ];
-          "video/x-msvideo" = [ video ]; # avi
-          "video/quicktime" = [ video ]; # mov
-          "video/mpeg" = [ video ];
+            "video/mp4" = [ video ];
+            "video/x-matroska" = [ video ]; # mkv
+            "video/webm" = [ video ];
+            "video/x-msvideo" = [ video ]; # avi
+            "video/quicktime" = [ video ]; # mov
+            "video/mpeg" = [ video ];
 
-          "audio/mpeg" = [ audio ]; # mp3
-          "audio/mp4" = [ audio ]; # m4a
-          "audio/ogg" = [ audio ];
-          "audio/flac" = [ audio ];
-          "audio/wav" = [ audio ];
-          "audio/x-wav" = [ audio ];
-          "audio/webm" = [ audio ];
-          "audio/aac" = [ audio ];
+            "audio/mpeg" = [ audio ]; # mp3
+            "audio/mp4" = [ audio ]; # m4a
+            "audio/ogg" = [ audio ];
+            "audio/flac" = [ audio ];
+            "audio/wav" = [ audio ];
+            "audio/x-wav" = [ audio ];
+            "audio/webm" = [ audio ];
+            "audio/aac" = [ audio ];
 
-          "application/zip" = [ archive ];
-          "application/x-zip-compressed" = [ archive ];
-          "application/x-7z-compressed" = [ archive ];
-          "application/x-rar" = [ archive ];
-          "application/x-rar-compressed" = [ archive ];
-          "application/x-tar" = [ archive ];
-          "application/gzip" = [ archive ];
-          "application/x-gzip" = [ archive ];
-          "application/x-bzip2" = [ archive ];
-          "application/x-xz" = [ archive ];
-          "application/zstd" = [ archive ];
-          "application/x-zstd" = [ archive ];
+            "application/zip" = [ archive ];
+            "application/x-zip-compressed" = [ archive ];
+            "application/x-7z-compressed" = [ archive ];
+            "application/x-rar" = [ archive ];
+            "application/x-rar-compressed" = [ archive ];
+            "application/x-tar" = [ archive ];
+            "application/gzip" = [ archive ];
+            "application/x-gzip" = [ archive ];
+            "application/x-bzip2" = [ archive ];
+            "application/x-xz" = [ archive ];
+            "application/zstd" = [ archive ];
+            "application/x-zstd" = [ archive ];
 
-          "application/vnd.openxmlformats-officedocument.wordprocessingml.document" = [ office ]; # docx
-          "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" = [ office ]; # xlsx
-          "application/vnd.openxmlformats-officedocument.presentationml.presentation" = [ office ]; # pptx
-          "application/msword" = [ office ];
-          "application/vnd.ms-excel" = [ office ];
-          "application/vnd.ms-powerpoint" = [ office ];
-          "application/vnd.oasis.opendocument.text" = [ office ]; # odt
-          "application/vnd.oasis.opendocument.spreadsheet" = [ office ]; # ods
-          "application/vnd.oasis.opendocument.presentation" = [ office ]; # odp
+            "application/vnd.openxmlformats-officedocument.wordprocessingml.document" = [ office ]; # docx
+            "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" = [ office ]; # xlsx
+            "application/vnd.openxmlformats-officedocument.presentationml.presentation" = [ office ]; # pptx
+            "application/msword" = [ office ];
+            "application/vnd.ms-excel" = [ office ];
+            "application/vnd.ms-powerpoint" = [ office ];
+            "application/vnd.oasis.opendocument.text" = [ office ]; # odt
+            "application/vnd.oasis.opendocument.spreadsheet" = [ office ]; # ods
+            "application/vnd.oasis.opendocument.presentation" = [ office ]; # odp
+          };
         };
       };
     };
