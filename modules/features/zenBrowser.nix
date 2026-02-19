6d38ce96534f177d203f54a1c5663c106ac0a4c0
Apollon002 <accounts@jannikzenker.de>
2026-02-19 12:21:54 +0100

    Added extensions and privacy settings to zenBrowser
    - Feat: Added extensions "Ublock Origin", "Bitwarden" and "Sponsorblock"
    zen-browser to be automatically installed
    - Config: Set default search engine for zen-browser to "DuckDuckGo"
    - Config: Disabled telemetry for zen-browser

--- modules/features/zenBrowser.nix
+++ modules/features/zenBrowser.nix
@@ -11,8 +11,41 @@
       pkgs,
       ...
     }:
+    let
+      extension = shortId: guid: {
+        name = guid;
+        value = {
+          install_url = "https://addons.mozilla.org/en-US/firefox/downloads/latest/${shortId}/latest.xpi";
+          installation_mode = "normal_installed";
+        };
+      };
+
+      extensions = [
+        # To add additional extensions, find it on addons.mozilla.org, find
+        # the short ID in the url (like https://addons.mozilla.org/en-US/firefox/addon/!SHORT_ID!/)
+        # Then go to https://addons.mozilla.org/api/v5/addons/addon/!SHORT_ID!/ to get the guid
+        (extension "ublock-origin" "uBlock0@raymondhill.net")
+        (extension "bitwarden-password-manager" "{446900e4-71c2-419f-a6a7-df9c091e268b}")
+        (extension "sponsorblock" "sponsorBlocker@ajay.app")
+      ];
+
+    in
     {
-      home.packages = [ inputs.zen-browser.packages.${pkgs.stdenv.hostPlatform.system}.default ];
       userSettings.defaultBrowser = "zen";
+      home.packages = [
+        (pkgs.wrapFirefox
+          inputs.zen-browser.packages.${pkgs.stdenv.hostPlatform.system}.zen-browser-unwrapped
+          {
+            extraPolicies = {
+              DisableTelemetry = true;
+              ExtensionSettings = builtins.listToAttrs extensions;
+
+              SearchEngines = {
+                Default = "DuckDuckGo";
+              };
+            };
+          }
+        )
+      ];
     };
 }
