c0be068dfef77b3b733db82b1acd2dd5a1d1b59c
Apollon002 <accounts@jannikzenker.de>
2026-02-19 15:40:19 +0100

    Added auto-microfetch in fish if microfetch is enabled
    - Feat: Microfetch will now automatically be executed if enabled in fish

--- modules/features/fish.nix
+++ modules/features/fish.nix
@@ -16,11 +16,14 @@
       {
         programs.fish = {
           enable = true;
-          shellInit =
-            ""
-            + (lib.optionalString (config.home.username == "jannik") ''
-              export SSH_AUTH_SOCK=/home/jannik/.bitwarden-ssh-agent.sock
-            '');
+          shellInit = ''
+            if type -q microfetch
+              microfetch
+            end
+          ''
+          + lib.optionalString (config.home.username == "jannik") ''
+            export SSH_AUTH_SOCK=/home/jannik/.bitwarden-ssh-agent.sock
+          '';
         };
       };
   };
